dd2b04c5cb828a3d20c4f75458065000d3cfd62d
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-02-13 21:11:46 -0300

    Add play function which set computer selection squares

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 const audio = document.querySelectorAll("audio");
-const square = document.querySelectorAll(".square");
+const squares = document.querySelectorAll(".square");
+const startButton = document.querySelector("#start");
+let userSelection = [];
+let computerSelection = [];
+let round = 0;
 
 function playSound() {
   for (let i = 0; i < 4; i++) {
-    square[i].onclick = function () {
+    squares[i].onclick = function () {
       audio[i].play();
     };
   }
 }
 
-playSound();
+function getRandomSquare() {
+  let index = Math.floor(Math.random() * 4);
+  return squares[index];
+}
+
+function highlightSquare(square) {
+  square.style.opacity = "1";
+  setTimeout(function () {
+    square.style.opacity = "0.4";
+  }, 500);
+}
+
+function play() {
+  let newSquare = getRandomSquare();
+  computerSelection.push(newSquare);
+
+  computerSelection.forEach(function (square, index) {
+    const delay= (index + 1) * 1000;
+    setTimeout(function () {
+      highlightSquare(square);
+      playSound(square);
+    }, delay);
+  });
+
+  round++;
+}
+
+startButton.onclick = function () {
+  userSelection = [];
+  computerSelection = [];
+  round = 0;
+
+  setInterval(play, 5000);
+};
